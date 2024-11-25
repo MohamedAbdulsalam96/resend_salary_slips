b8307f0021d12c9faa908889ed3becef4520ea8d
dawoodjee <Adam Dawoodjee>
2024-11-25 16:51:50 +0000

    first commit

---
+++ resend_salary_slips/app.py
@@ -0,0 +1,59 @@
+import frappe
+from frappe.query_builder.functions import Coalesce
+
+@frappe.whitelist()
+def email_salary_slips(payroll_entry_name, publish_progress=True):
+    print('payroll_entry_name', payroll_entry_name)
+    payroll_entry = frappe.get_doc("Payroll Entry", payroll_entry_name)
+    salary_slips = get_sal_slip_list(payroll_entry, ss_status=1)
+
+    if salary_slips:
+
+        emailed_employees = []
+        failed_employees = []
+
+        
+        for ss in salary_slips:
+            if not frappe.get_all(
+                "Email Queue", 
+                filters={"reference_doctype": "Salary Slip", "reference_name": ss.name, "status": "Not Sent"},
+                fields=["name"], limit=1
+            ):
+                slip = frappe.get_doc("Salary Slip", ss.name)
+                # Fetch employee's preferred email
+                prefered_email = frappe.db.get_value("Employee", slip.employee, "prefered_email", cache=True)
+                
+                if prefered_email:
+                    # If the employee has a preferred email, send the salary slip
+                    slip.email_salary_slip()
+                    emailed_employees.append(slip.employee_name)  # Add to the emailed list
+                else:
+                    # If no email is found, add to the failed list
+                    failed_employees.append(slip.employee_name)  # Add to the failed list
+
+        # After processing, print out the results
+
+        if emailed_employees:        
+            frappe.msgprint(f"Salary Slips emailed to: {', '.join(emailed_employees)}")
+        if failed_employees:
+            frappe.msgprint(f"Add a <strong>Prefered Email</strong> is for {', '.join(failed_employees)} and try again")
+
+def get_sal_slip_list(pe, ss_status, as_dict=True):
+    """
+    Returns list of salary slips based on selected criteria
+    """
+
+    ss = frappe.qb.DocType("Salary Slip")
+    ss_list = (
+        frappe.qb.from_(ss)
+        .select(ss.name, ss.salary_structure)
+        .where(
+            (ss.docstatus == ss_status)
+            & (ss.start_date >= pe.start_date)
+            & (ss.end_date <= pe.end_date)
+            & (ss.payroll_entry == pe.name)
+            & (Coalesce(ss.salary_slip_based_on_timesheet, 0) == pe.salary_slip_based_on_timesheet)
+        )
+    ).run(as_dict=as_dict)
+
+    return ss_list
